ec063b382a21e01c4c25d65001e264c81e99f275
ISHU <raoishu5600@gmail.com>
2024-03-25 17:39:31 +0530

    First commit

---
+++ javaScript.js
@@ -0,0 +1,26 @@
+const randomColor = function(){
+  const hex = '0123456789ABCDEF'  ;
+  let color = "#" ;
+  for(let i=0; i<6; i++){
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+let intervalID ;
+const startChangingColor = function(){
+  intervalID = setInterval(changeBgColor, 1000 );
+  
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor()  ;
+  }
+};
+
+const stopChangingColor = function(){
+  clearInterval(intervalID) ;
+  intervalID = null ;
+};
+
+document.querySelector("#start").addEventListener('click', startChangingColor)
+
+document.querySelector("#stop").addEventListener('click', stopChangingColor)
